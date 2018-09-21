8c9d1d927c3e9432716ff46191fb10d4e279ffe2
Matthew Waymouth <matthewwaymouth@Matthews-MacBook-Pro.local>
2018-09-20 19:50:52 -0600

    Colored Lorenz Attractor, Stil need to add text at the bottom

---
+++ HW2.c
@@ -0,0 +1,211 @@
+/*
+ * Simple program to demonstrate generating coordinates
+ * using the Lorenz Attractor
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <stdarg.h>
+ //  OpenGL with prototypes for glext
+ #define GL_GLEXT_PROTOTYPES
+ #ifdef __APPLE__
+ #include <GLUT/glut.h>
+ #else
+ #include <GL/glut.h>
+ #endif
+
+// Global Vars
+int th=0;
+int ph=0;
+double dim=100;
+//char* text[];
+
+
+/*  Lorenz Parameters  */
+double s  = 10;
+double b  = 2.6666;
+double r  = 28;
+
+/*
+ *  Convenience routine to output raster text
+ *  Use VARARGS to make this more flexible
+ */
+#define LEN 8192  // Maximum length of text string
+void Print(const char* format , ...)
+{
+   char    buf[LEN];
+   char*   ch=buf;
+   va_list args;
+   //  Turn the parameters into a character string
+   va_start(args,format);
+   vsnprintf(buf,LEN,format,args);
+   va_end(args);
+   //  Display the characters one at a time at the current raster position
+   while (*ch)
+      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*ch++);
+}
+
+/*
+ *  Display the scene
+ */
+void display()
+{
+   //  Clear the image
+   glClear(GL_COLOR_BUFFER_BIT);
+   //  Reset previous transforms
+   glLoadIdentity();
+   //  Set view angle
+   glRotated(ph,1000,0,0);
+   glRotated(th,0,1000,0);
+
+
+   int i;
+   /*  Coordinates  */
+   double x = 1;
+   double y = 1;
+   double z = 1;
+   /*  Time step  */
+   double dt = 0.001;
+   /*
+    *  Integrate 50,000 steps (50 time units with dt = 0.001)
+    *  Explicit Euler integration
+    */
+    //  Draw yellow lines
+    glColor3f(1,1,0);
+    glBegin(GL_LINE_STRIP);
+   for (i=0;i<50000;i++)
+   {
+      double dx = s*(y-x);
+      double dy = x*(r-z)-y;
+      double dz = x*y - b*z;
+      x += dt*dx;
+      y += dt*dy;
+      z += dt*dz;
+      if (i == 20000)
+        glColor3f(1,0,0);
+      // else if (i == 20000)
+      //   glColor3f(1,0,1);
+      // else if (i == 30000)
+      //   glColor3f(0,0,1);
+      else if (i == 40000)
+        glColor3f(0,1,1);
+      //printf("%5d %8.3f %8.3f %8.3f\n",i+1,x,y,z);
+      glVertex3d(x,y,z);
+   }
+   glEnd();
+   //  Draw axes in white
+   glColor3f(1,1,1);
+   glBegin(GL_LINES);
+   glVertex3d(0,0,0);
+   glVertex3d(65,0,0);
+   glVertex3d(0,0,0);
+   glVertex3d(0,65,0);
+   glVertex3d(0,0,0);
+   glVertex3d(0,0,65);
+   glEnd();
+   //  Label axes
+   glRasterPos3d(65,0,0);
+   Print("X");
+   glRasterPos3d(0,65,0);
+   Print("Y");
+   glRasterPos3d(0,0,65);
+   Print("Z");
+   //  Display parameters
+   // glWindowPos2i(5,5);
+   // Print("View Angle=%d,%d  %s",th,ph,text[mode]);
+   // if (mode==2)
+   //    Print("  z=%.1f",z);
+   // else if (mode==4)
+   //    Print("  w=%.1f",w);
+   //  Flush and swap
+   glFlush();
+   glutSwapBuffers();
+}
+
+
+void key(unsigned char ch, int x, int y)
+{
+  // Exit on ESC
+  if (ch == 27)
+    exit(0);
+  else if (ch == '+')
+    r += 0.5;
+  else if (ch == '-')
+    r -= 0.5;
+  //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when an arrow key is pressed
+ */
+void special(int key,int x,int y)
+{
+   //  Right arrow key - increase azimuth by 5 degrees
+   if (key == GLUT_KEY_RIGHT)
+      th += 5;
+   //  Left arrow key - decrease azimuth by 5 degrees
+   else if (key == GLUT_KEY_LEFT)
+      th -= 5;
+   //  Up arrow key - increase elevation by 5 degrees
+   else if (key == GLUT_KEY_UP)
+      ph += 5;
+   //  Down arrow key - decrease elevation by 5 degrees
+   else if (key == GLUT_KEY_DOWN)
+      ph -= 5;
+   //  Keep angles to +/-360 degrees
+   th %= 360;
+   ph %= 360;
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when the window is resized
+ */
+void reshape(int width,int height)
+{
+   //  Ratio of the width to the height of the window
+   double w2h = (height>0) ? (double)width/height : 1;
+   //  Set the viewport to the entire window
+   glViewport(0,0, width,height);
+   //  Tell OpenGL we want to manipulate the projection matrix
+   glMatrixMode(GL_PROJECTION);
+   //  Undo previous transformations
+   glLoadIdentity();
+   //  Orthogonal projection box adjusted for the
+   //  aspect ratio of the window
+   glOrtho(-dim*w2h,+dim*w2h, -dim,+dim, -dim,+dim);
+   //  Switch to manipulating the model matrix
+   glMatrixMode(GL_MODELVIEW);
+   //  Undo previous transformations
+   glLoadIdentity();
+}
+
+
+/*
+ *  Main
+ */
+int main(int argc, char *argv[])
+{
+  //  Initialize GLUT and process user parameters
+  glutInit(&argc,argv);
+  //  Request double buffered, true color window
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+  //  Request 500 x 500 pixel window
+  glutInitWindowSize(550,550);
+  //  Create the window
+  glutCreateWindow("Assignment 2: Matt Waymouth");
+  //  Tell GLUT to call "display" when the scene should be drawn
+  glutDisplayFunc(display);
+ //  Tell GLUT to call "reshape" when the window is resized
+  glutReshapeFunc(reshape);
+  //  Tell GLUT to call "special" when an arrow key is pressed
+  glutSpecialFunc(special);
+  //  Tell GLUT to call "key" when a key is pressed
+  glutKeyboardFunc(key);
+  //  Pass control to GLUT so it can interact with the user
+  glutMainLoop();
+   // Return 0
+   return 0;
+}
